2c570cc4aaa4c0ee8d0a8921398713939aab1fdc
ZSteh <72886358+ZSteh@users.noreply.github.com>
2020-11-03 15:24:55 +0200

    Update App.js
    hello world

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Hello World from Group 9 : Zane Stukena! </Text>
+      <Text>This is my Zanes Stukena' first React Native application!</Text>
       <StatusBar style="auto" />
     </View>
   );
